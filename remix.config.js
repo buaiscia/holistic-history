6a2ca68a9cf2d647e5cbab851e5ccd8238a0062c
Alex Buaiscia <alex.buaiscia@gmail.com>
2023-11-25 21:43:57 +0100

    Upgrade Remix and update configurations

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
+  serverModuleFormat: "cjs",
 };
